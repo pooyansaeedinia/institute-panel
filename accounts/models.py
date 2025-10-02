368f7ba71187cf1082571d63eaee49dd79080f09
pooyan saeedinia <pooyansaeedinia@gmail.com>
2025-10-02 11:46:57 +0330

    CustomUser has been updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    user_code = models.CharField(
+        editable=False,
+        unique=True,
+    )
+
+    national_code = models.CharField(
+        max_length=10,
+        unique=True,
+    )
+
     EDUCATIONAL_GROUP_CHOICES = [
         ('teacher', 'teacher'),
         ('student', 'student'),
@@ -19,6 +30,13 @@ class CustomUser(AbstractUser):
         null=True,
     )
 
+    Tel = models.CharField(
+        max_length=12,
+        unique=True,
+    )
+
+    birth_date = models.DateField()
+
     GENDER_CHOICES = [
         ('male', 'male'),
         ('female', 'female'),
@@ -32,3 +50,16 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+    def code_generator(self):
+        username_numbers = str(ord(self.username[0])) + str(ord(self.username[-1]))
+        national_code_number = str(self.national_code)[:3]
+        phone_number_number = str(self.Tel)[8:]
+        birth_date_number = str(self.birth_date)[6] + str(self.birth_date)[8:]
+        full_code = username_numbers + national_code_number + phone_number_number + birth_date_number
+        return full_code
+
+    def save(self, *args, **kwargs):
+        if not self.user_code:
+            self.user_code = self.code_generator()
+        super().save(*args, **kwargs)
